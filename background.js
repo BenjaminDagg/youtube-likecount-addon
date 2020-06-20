9433af1fd0f3052b10cc9e5dab4c54045e4703d6
unknown <bdagg@csumb.edu>
2020-06-20 03:57:43 -0700

    removed un needed files and lines of code

--- background.js
+++ background.js
@@ -1,10 +1,9 @@
 chrome.runtime.onInstalled.addListener(function() {
     // ...
     
+    //listener for when url on current tab changes
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-      // changeInfo object: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/onUpdated#changeInfo
-      // status is more reliable (in my case)
-      // use "alert(JSON.stringify(changeInfo))" to check what's available and works in your case
+      
       if (changeInfo.status === 'complete') {
         chrome.tabs.sendMessage(tabId, {
           message: 'TabUpdated'
@@ -12,6 +11,4 @@ chrome.runtime.onInstalled.addListener(function() {
       }
     })
     
-    
-   
   });
--- script.js
+++ script.js
@@ -1,161 +1,111 @@
 
 
 
-/*
-let anchors = document.getElementsByTagName('a');
-setInterval(function() {
-    anchors = document.getElementsByTagName('a');
-},2000)
-var links = [];
-for (var i = 0; i < anchors.length;i++) {
-    if (anchors[i].id.includes('thumbnail')) {
+/* detects when url changes in the current tab */
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.message === 'TabUpdated') {
+        setTimeout(function() {
+            start();
+        },3000)
+      
+    }
+  })
+
+
+/* gets all youtube video containers and appends the likebar onto it */
+function start() {
+    //find all links
+    let anchors = document.getElementsByTagName('a');
+    var links = [];
+    //keep only links that go to youtube videos to see which containers have videos
+    for (var i = 0; i < anchors.length;i++) {
+        if (anchors[i].id.includes('thumbnail')) {
         links.push(anchors[i])
-    }
-}
-
-var containers = [];
-for (var i = 0; i < links.length;i++) {
-    var infoTab = document.createElement('div');
-    infoTab.className = 'info-tab';
-    infoTab.innerHTML = "hello";
-    var container = links[i].parentElement.parentElement;
-    containers.push(container);
-    links[i].parentElement.parentElement.appendChild(infoTab);
-}
-
-var videoContainers = document.getElementsByTagName('ytd-rich-grid-video-renderer');
-var horizContainers = document.getElementsByTagName('ytd-video-renderer');
-videoContainers = videoContainers + horizContainers;
-for (var i = 0;  i < videoContainers.length;i++) {
-    //console.log(containers[i]);
-    containers[i].addEventListener("mouseenter", onVideoHover);
-}
-for (var i = 0;  i < containers.length;i++) {
-    //console.log(containers[i]);
-    containers[i].addEventListener("mouseenter", onVideoHover);
-}
-
-function onVideoHover(e) {
-    var item = e.srcElement;
-    console.log(item);
-    var infotab = item.querySelector('.info-tab');
-    
-}
-
-/*
-var oldURL = "";
-var currentURL = window.location.href;
-function checkURLchange(currentURL){
-    if(currentURL != oldURL){
-        init();
-        oldURL = currentURL;
-        
+        }
     }
 
-    oldURL = window.location.href;
-    setInterval(function() {
-        checkURLchange(window.location.href);
-    }, 2000);
-}
-
-checkURLchange();
-*/
-
-/*
-let changeColor = document.getElementById('changeColor');
-let detailsContainers = document.getElementsByClassName('style-scope ytd-video-meta-block');
-let anchors = document.getElementsByTagName('a');
-
-var links = [];
-for (var i = 0; i < anchors.length;i++) {
-    if (anchors[i].id.includes('thumbnail')) {
-        links.push(anchors[i])
-    }
-}
-//console.log(links);
-
-const API_KEY = 'AIzaSyDaFaHT1cF3mHHLgwZoicvlvJCfv5FKKZo';
-
-document.addEventListener('DOMContentLoaded', function () {
-    alert('done');
-});
-
-    
-
-for (var i = 0; i < links.length;i++) {
-    var parent = links[i].parentElement.parentElement;
-    var test = document.createElement('span');
-    test.innerHTML = "this is a test";
-    //console.log(links[i].href);
-    
-    
-    getData(links[i].href.substr(32,links[i].href.length));
-}
-
-function init() {
-    //console.log('done');
-    let changeColor = document.getElementById('changeColor');
-let detailsContainers = document.getElementsByClassName('style-scope ytd-video-meta-block');
-let anchors = document.getElementsByTagName('a');
-
-var links = [];
-for (var i = 0; i < anchors.length;i++) {
-    if (anchors[i].id.includes('thumbnail')) {
-        links.push(anchors[i])
-    }
-}
+    //append the likebar div onto each container (may need to update as youtube changes)
+    var containers = [];
     for (var i = 0; i < links.length;i++) {
+        
+        //likebar div
+        var infoTab = document.createElement('div');
+        infoTab.className = 'info-tab';
+        infoTab.innerHTML = '';
+
+        //console.log(links[i].parentElement.parentElement);
         var parent = links[i].parentElement.parentElement;
-        var test = document.createElement('div');
-        test.innerHTML = "this is a test";
-        //console.log(links[i].href);
         
-        
-        getData(links[i].href.substr(32,links[i].href.length));
+        //target div to put the likebar
+        var container = links[i].parentElement.parentElement;
+        containers.push(container);
+        links[i].parentElement.parentElement.appendChild(infoTab);
+    }
+
+    //selects all tags that house a youtube video and attach a on hover listener
+    var videoContainers = document.getElementsByTagName('ytd-rich-grid-video-renderer');
+    var horizContainers = document.getElementsByTagName('ytd-video-renderer');
+    
+    for (var i = 0;  i < videoContainers.length;i++) {
+        //console.log(containers[i]);
+        videoContainers[i].addEventListener("mouseenter", onVideoHover);
+    }
+    for (var i = 0;  i < horizContainers.length;i++) {
+        //console.log(containers[i]);
+        horizContainers[i].addEventListener("mouseenter", onVideoHover);
     }
 }
 
 
-async function getData(id) {
-    let changeColor = document.getElementById('changeColor');
-let detailsContainers = document.getElementsByClassName('style-scope ytd-video-meta-block');
-let anchors = document.getElementsByTagName('a');
+//function called when mouse hovers over video
+async function onVideoHover(e) {
 
-var links = [];
-for (var i = 0; i < anchors.length;i++) {
-    if (anchors[i].id.includes('thumbnail')) {
-        links.push(anchors[i])
-    }
+    //the item the you are hovering over (that triggered the event)
+    var item = e.srcElement;
+    //console.log(item);
+    //next div in the tree
+    var dismissable = item.querySelector('#dismissable');
+    //child of dism
+    var link = dismissable.querySelector('#thumbnail');
+    //console.log(link);
+    //likebar that was added earlier
+    var infoTab = dismissable.querySelector('.info-tab');
+    //https://www.youtube.com/watch?v=rI1ZXRJrql8
+
+    //get url from the link
+    var id = link.href.substr(32,link.href.length);
+
+    //call api to get like/dislike info
+    var data = await fetch('https://yas-dagg.herokuapp.com/' + id)
+    var json = await data.json();
+
+
+    var likeCount = parseInt(json.data.items[0].statistics.likeCount);
+    var dislikeCount = parseInt(json.data.items[0].statistics.dislikeCount);
+    var total = likeCount + dislikeCount;
+
+    //actual likebar itself
+    var totalDiv = document.createElement('div');
+    totalDiv.className = 'likebar';
+
+    //like (green portion of likebar)
+    var likeDiv = document.createElement('div');
+    likeDiv.className = 'like-div';
+    likeDiv.style.flex = (likeCount / total) * 100 + '%';
+
+    //dislike (red ) portion
+    var disDiv = document.createElement('div');
+    disDiv.className = 'dislike-div';
+    disDiv.style.flex = (dislikeCount / total) * 100 + '%';
+
+    //put like/dislike count on likebar div
+    infoTab.innerHTML = '👍: ' + json.data.items[0].statistics.likeCount +  ' 👎 ' + json.data.items[0].statistics.dislikeCount;
+
+    //add likebar div to container
+    totalDiv.appendChild(likeDiv);
+    totalDiv.appendChild(disDiv);
+    infoTab.appendChild(totalDiv);
+    
 }
-        var item = null;
-        for (var i = 0; i < links.length;i++) {
-            if (links[i].href.substr(32,links[i].href.length) === id) {
-                item = links[i];
-            }
-        }
-        var data = await fetch('http://localhost:3000/' + id)
-        var json = await data.json();
-        var test = document.createElement('p');
-        var parent = item.parentElement.parentElement;
-        test.className = 'likebar';
-        
-        test.innerHTML = json.data.items[0].statistics.likeCount;
-        //console.log(json);
-        //var parent = item.parentElement.parentElement.querySelector('#details').querySelector('#meta');
-        
-        for (var i = 0; i < parent.childNodes.length;i++) {
-            if (parent.childNodes[i].className == 'likebar') {
-                parent.removeChild(parent.childNodes[i])
-            }
-        }
-        
-        
-        
-       parent.appendChild(test);
-        
 
-        return json.data;
 
-}
-
-*/
